9c3673aff3451ce6fd755c0514bed0be83021a91
Joshua Nougaret <nougaret.j@northeastern.edu>
2023-07-23 16:48:28 -0400

    up to 2.2.6.3

---
+++ src/labs/a3/working-with-functions.js
@@ -0,0 +1,12 @@
+import ES5Functions from "./es5-functions";
+import ArrowFunctions from "./arrow-functions";
+
+function WorkingWithFunctions() {
+    return (
+        <>
+            <ES5Functions />
+            <ArrowFunctions />
+        </>
+    );
+}
+export default WorkingWithFunctions
