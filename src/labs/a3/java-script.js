29073e7409b4734b6a0c363b12dd47d139adc90f
Joshua Nougaret <nougaret.j@northeastern.edu>
2023-07-23 21:46:02 -0400

    up to 2.3

--- src/labs/a3/java-script.js
+++ src/labs/a3/java-script.js
@@ -4,6 +4,12 @@ import BooleanVariables from "./boolean-variables";
 import IfElse from "./if-else";
 import TernaryOperator from "./ternary-operator";
 import WorkingWithFunctions from "./working-with-functions";
+import WorkingWithArrays from "./working-with-arrays";
+import TemplateLiterals from "./template-literals";
+import House from "./house";
+import Spread from "./spread";
+import Destructing from "./destructing";
+import FunctionDestructing from "./function-destructing";
 
 function JavaScript() {
     console.log('Hello World!');
@@ -16,6 +22,12 @@ function JavaScript() {
             <IfElse />
             <TernaryOperator />
             <WorkingWithFunctions />
+            <WorkingWithArrays />
+            <TemplateLiterals />
+            <House />
+            <Spread />
+            <Destructing />
+            <FunctionDestructing />
         </div>
     );
 }
