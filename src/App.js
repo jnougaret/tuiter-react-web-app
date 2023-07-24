0f75ed355e8831c23380c5c4082d885e04801aea
Joshua Nougaret <nougaret.j@northeastern.edu>
2023-07-24 09:57:05 -0400

    up to 2.3.2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a3/hello-world";
 import Tuiter from "./tuiter";
+import { HashRouter } from "react-router-dom";
+import {Routes, Route} from "react-router";
 
 function App() {
-   return (
-        <div className="container">
-            <HelloWorld/>
-            <Labs />
-            <Tuiter/>
-        </div>
+    return (
+       <HashRouter>
+            <div className="container">
+                <Routes>
+                    <Route path="/"         element={<Navigate to="/labs"/>}/>
+                    <Route path="/labs/*"   element={<Labs/>}/>
+                    <Route path="/hello"    element={<HelloWorld/>}/>
+                    <Route path="/tuiter/*" element={<Tuiter/>}/>
+                </Routes>
+            </div>
+       </HashRouter>
     );
 }
 export default App;
