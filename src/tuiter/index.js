1336096b47edc11d8361d48351c68bd8664d2454
Joshua Nougaret <nougaret.j@northeastern.edu>
2023-07-24 12:39:58 -0400

    up to 3.4

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,11 +1,33 @@
+import { Routes, Route } from "react-router";
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from "./who-to-follow-list";
+import HomeScreen from "./navigation-sidebar/home-screen";
+import ExploreScreen from "./navigation-sidebar/explore-screen";
+import BookmarksScreen from "./navigation-sidebar/bookmarks-screen";
+import ProfileScreen from "./navigation-sidebar/profile-screen";
 
 function Tuiter() {
-   return(
-      <div>
-         <Nav />
-         <h1>Tuiter</h1>
-      </div>
-   );
+ return (
+   <div>
+     <Nav />
+     <div className="row">
+       <div className="col-2">
+         <NavigationSidebar />
+       </div>
+       <div className="col-7">
+         <Routes>
+            <Route path="/home" element={<HomeScreen />} />
+            <Route path="/explore" element={<ExploreScreen />} />
+            <Route path="/bookmarks" element={<BookmarksScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+         </Routes>
+       </div>
+       <div className="col-3">
+             <WhoToFollowList />
+       </div>
+     </div>
+   </div>
+ );
 }
-export default Tuiter
+export default Tuiter;
