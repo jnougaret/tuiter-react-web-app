c917da37f0dd5e5e3996c86245c42292e1c0d6a8
Joshua Nougaret <nougaret.j@northeastern.edu>
2023-07-24 11:02:54 -0400

    up to 2.4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Labs from "./labs";
 import HelloWorld from "./labs/a3/hello-world";
 import Tuiter from "./tuiter";
 import { HashRouter } from "react-router-dom";
-import {Routes, Route} from "react-router";
+import { Routes, Route } from "react-router";
+import { Navigate } from "react-router";
 
 function App() {
     return (
--- src/labs/a3/hello-world.js
+++ src/labs/a3/hello-world.js
@@ -1,10 +1,9 @@
-import {Link} from "react-router-dom";
+import Nav from "../../nav";
+
 function HelloWorld() {
    return(
         <div>
-            <Link to="/labs/a3">A3</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
+           <Nav />
             <h1>Hello World!</h1>
         </div>
    );
