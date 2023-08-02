91ba360fc5a63f8656054bcc7938c611880f37a0
Joshua Nougaret <nougaret.j@northeastern.edu>
2023-08-01 21:52:47 -0400

    updates

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route } from "react-router";
 import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
@@ -6,28 +7,35 @@ import HomeScreen from "./navigation-sidebar/home-screen";
 import BookmarksScreen from "./navigation-sidebar/bookmarks-screen";
 import ProfileScreen from "./navigation-sidebar/profile-screen";
 import ExploreScreen from "./explore-screen";
+import whoReducer from "./reducers/who-reducer";
+import { configureStore } from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+  {reducer: {who: whoReducer}});
 
 function Tuiter() {
- return (
-   <div>
-     <Nav />
-     <div className="row">
-       <div className="col-2">
-         <NavigationSidebar />
-       </div>
-       <div className="col-7">
-         <Routes>
-            <Route path="/home" element={<HomeScreen />} />
-            <Route path="/explore" element={<ExploreScreen />} />
-            <Route path="/bookmarks" element={<BookmarksScreen />} />
-            <Route path="/profile" element={<ProfileScreen />} />
-         </Routes>
-       </div>
-       <div className="col-3">
-          <WhoToFollowList />
-       </div>
-     </div>
-   </div>
+  return (
+   <Provider store={store}>
+      <div>
+        <Nav />
+        <div className="row">
+          <div className="d-block col-4 col-sm-3 col-md-3 col-lg-2 col-xl-2 col-xxl-2">
+            <NavigationSidebar />
+          </div>
+          <div className="d-block col-8 col-sm-9 col-md-9 col-lg-6 col-xl-7 col-xxl-7">
+            <Routes>
+              <Route path="/home" element={<HomeScreen />} />
+              <Route path="/explore" element={<ExploreScreen />} />
+              <Route path="/bookmarks" element={<BookmarksScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
+            </Routes>
+          </div>
+          <div className="d-none d-lg-block col-lg-4 col-xl-3 col-xxl-3">
+            <WhoToFollowList />
+          </div>
+        </div>
+      </div>
+   </Provider>
  );
 }
 export default Tuiter;
